79b7052b35faf457bbe0a61d36edc51b9967808a
DESKTOP-STACPPF\Bui Tuan <tuanbui2112@gmail.com>
2022-09-29 21:03:09 +0700

    pagi

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,4 +1,4 @@
-const productNewarrivals =[
+const productNewarrivals = [
   {
     "id": 8,
     "name": "Brinjal",
@@ -58,10 +58,10 @@ const productNewarrivals =[
 ]
 
 renderPrn(productNewarrivals)
-function renderPrn(products){
-    let item =""
-    for(let product of products){
-        item += `
+function renderPrn(products) {
+  let item = ""
+  for (let product of products) {
+    item += `
         <div class="bestSeller_tabContent--item col-lg-3 col-md-4 col-sm-6 col-6">
         <div class="tabContent_item--image">
         <a href="#">
@@ -105,10 +105,11 @@ function renderPrn(products){
             </p>
             </div>
             `
-    }
-    $('.bestSeller_tabContent--a1 .row ').html(item)
+
+  }
+  $('.bestSeller_tabContent--a1 .row ').html(item)
 }
-const productBestSeller =[
+const productBestSeller = [
   {
     "id": 16,
     "name": "Banana",
@@ -167,10 +168,10 @@ const productBestSeller =[
   },
 ]
 renderPrb(productBestSeller)
-function renderPrb(products){
-    let item =""
-    for(let product of products){
-        item += `
+function renderPrb(products) {
+  let item = ""
+  for (let product of products) {
+    item += `
         <div class="bestSeller_tabContent--item col-lg-3 col-md-4 col-sm-6 col-6">
         <div class="tabContent_item--image">
         <a href="#">
@@ -214,118 +215,120 @@ function renderPrb(products){
             </p>
             </div>
             `
-    }
-    $('.bestSeller_tabContent--a2 .row ').html(item)
+  }
+  $('.bestSeller_tabContent--a2 .row ').html(item)
 }
 
-const productShopPage =[
-  [
-    {
-      "id": 1,
-      "name": "Brocolli - 1 Kg",
-      "price": 120,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/broccoli.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 2,
-      "name": "Cauliflower - 1 Kg",
-      "price": 60,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cauliflower.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 3,
-      "name": "Cucumber - 1 Kg",
-      "price": 48,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cucumber.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 4,
-      "name": "Beetroot - 1 Kg",
-      "price": 32,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/beetroot.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 5,
-      "name": "Carrot - 1 Kg",
-      "price": 56,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/carrots.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 6,
-      "name": "Tomato - 1 Kg",
-      "price": 16,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/tomato.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 7,
-      "name": "Beans - 1 Kg",
-      "price": 82,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/beans.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 8,
-      "name": "Brinjal - 1 Kg",
-      "price": 35,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/brinjal.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 9,
-      "name": "Capsicum",
-      "price": 60,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/capsicum.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 10,
-      "name": "Mushroom - 1 Kg",
-      "price": 75,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/button-mushroom.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 11,
-      "name": "Potato - 1 Kg",
-      "price": 22,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/potato.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 12,
-      "name": "Pumpkin - 1 Kg",
-      "price": 48,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/pumpkin.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 13,
-      "name": "Corn - 1 Kg",
-      "price": 75,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/corn.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 14,
-      "name": "Onion - 1 Kg",
-      "price": 16,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/onion.jpg",
-      "category": "vegetables"
-    },
-    {
-      "id": 15,
-      "name": "Apple - 1 Kg",
-      "price": 72,
-      "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/apple.jpg",
-      "category": "fruits"
-    },
-    {
+
+// Render Product Shop Page
+const productShopPage = [
+
+  {
+    "id": 1,
+    "name": "Brocolli",
+    "price": 120,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/broccoli.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 2,
+    "name": "Cauliflower",
+    "price": 60,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cauliflower.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 3,
+    "name": "Cucumber",
+    "price": 48,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cucumber.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 4,
+    "name": "Beetroot",
+    "price": 32,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/beetroot.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 5,
+    "name": "Carrot",
+    "price": 56,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/carrots.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 6,
+    "name": "Tomato",
+    "price": 16,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/tomato.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 7,
+    "name": "Beans",
+    "price": 82,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/beans.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 8,
+    "name": "Brinjal",
+    "price": 35,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/brinjal.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 9,
+    "name": "Capsicum",
+    "price": 60,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/capsicum.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 10,
+    "name": "Mushroom",
+    "price": 75,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/button-mushroom.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 11,
+    "name": "Potato",
+    "price": 22,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/potato.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 12,
+    "name": "Pumpkin",
+    "price": 48,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/pumpkin.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 13,
+    "name": "Corn",
+    "price": 75,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/corn.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 14,
+    "name": "Onion",
+    "price": 16,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/onion.jpg",
+    "category": "vegetables"
+  },
+  {
+    "id": 15,
+    "name": "Apple",
+    "price": 72,
+    "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/apple.jpg",
+    "category": "fruits"
+  },
+  {
     "id": 16,
     "name": "Banana",
     "price": 45,
@@ -382,169 +385,242 @@ const productShopPage =[
     "category": "fruits"
   }
 ]
-]
+// next Page
+let perPage = 9
+let currentPage = 1
+let startPage = 0
+let endPage = perPage
+const totalPages = Math.ceil(productShopPage.length / perPage)
+console.log(totalPages);
+const nextPage = $('.next')
+const prevPage = $('.prev')
+
+//  get current PAge
+function getCurrentPage(currentPage){
+  startPage = (currentPage - 1) * perPage
+  endPage = currentPage * perPage
+}
+
+// next Page
+nextPage.on("click", function () {
+  currentPage++;
+  if (currentPage > totalPages) {
+    currentPage = totalPages
+  }
+  $('.pagination .page-items .page-link').removeClass('activePagi')
+  $(`.pagination .page-items:eq(${currentPage - 1}) .num`).addClass('activePagi')
+  getCurrentPage(currentPage)
+  renderPrSb(productShopPage)
+})
+// Previous Page
+prevPage.on("click", function () {
+  currentPage--;
+  if (currentPage <= 1) {
+    currentPage = 1
+  }
+  $('.pagination .page-items .page-link').removeClass('activePagi')
+  $(`.pagination .page-items:eq(${currentPage - 1}) .num`).addClass('activePagi')
+  getCurrentPage(currentPage)
+  renderPrSb(productShopPage)
+})
+// Change Page
+function changePage(){
+  const currentPages = document.querySelectorAll('.page-items') 
+  for(let i = 0;i < currentPages.length; i++)
+  {
+    currentPages[i].addEventListener("click",function(){
+      const value = i + 1
+      currentPage = value
+      $('.pagination .page-items .page-link').removeClass('activePagi')
+      currentPages[i].children[0].classList.add('activePagi')
+      getCurrentPage(currentPage)
+      renderPrSb(productShopPage) 
+    })
+  }
+}
+// ========================
 renderPrSb(productShopPage)
-function renderPrSb(products){
-    let item =""
-    for(let product of products){
-        item += `
-        <div class="bestSeller_tabContent--item col-11">
-        <div class="tabContent_item--image">
-        <a href="#">
-        <img src="${product.image}" alt="">
-        </a>
-        </div>
-        <div class="tabContent_item--options">
+function renderPrSb(products) {
+  let item = ""
+  for (let product of products) {
+    if (product.id > startPage && product.id < endPage) {
+
+      item += `
+        <div class="bestSeller_tabContent--item col-lg-3 col-md-4 col-sm-6 col-6 shopItemsList">
+            <!-- image Product -->
+            <div class="tabContent_item--image">
+              <a href="#!">
+                <img src="${product.image}" alt="">
+              </a>
+            </div>
+            <!-- end of image -->
+            <!-- choose options product -->
+            <div class="tabContent_item--options" >
               <ul class="item_options--main">
                 <li class="item_options" id="view">
-                  <a href="#" class="item_options--logo">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                      </svg>
-                    </a>
-                    <span class="item_options--Title" id="viewTitle">View</span>
-                      </li>
-                      <li class="item_options" id="wish">
-                    <a href="#" class="item_options--logo"> 
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-                      </svg>
-                    </a>
-                    <span class="item_options--Title" id = "wishTitle">Add to WishList</span>
-                      </li>
-                      <li class="item_options" id="cart">
-                    <a href="#" class="item_options--logo">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag-dash" viewBox="0 0 16 16">
-                      <path fill-rule="evenodd" d="M5.5 10a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z"/>
-                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                  <a href="#!" class="item_options--logo">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                      class="bi bi-search" viewBox="0 0 16 16">
+                      <path
+                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                     </svg>
-                    </a>
-                    <span class="item_options--Title" id = "cartTitle">Add to Cart</span>
-                      </li>
+                  </a>
+                  <span class="item_options--Title" id="viewTitle">View</span>
+                </li>
+                <li class="item_options" id="wish">
+                  <a href="#!" class="item_options--logo">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                      class="bi bi-heart" viewBox="0 0 16 16">
+                      <path
+                        d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                    </svg>
+                  </a>
+                  <span class="item_options--Title" id="wishTitle">Add to WishList</span>
+                </li>
+                <li class="item_options" id="cart">
+                  <a href="#!" class="item_options--logo">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                      class="bi bi-bag-dash" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M5.5 10a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z" />
+                      <path
+                        d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg>
+                  </a>
+                  <span class="item_options--Title" id="cartTitle">Add to Cart</span>
+                </li>
               </ul>
             </div>
-            <h3 class="tabContent_item--name">
-            <a href="#">${product.name}</a>
-            </h3>
-            <p class="tabContent_item--price">
-            $${product.price}.00
+            <!-- end of options -->
+            <!-- name product -->
+            <p class="tabContent_item--name" data-name="${product.name}">
+              ${product.name}
             </p>
-            </div>
+            <!-- end of name product-->
+            <!-- price product -->
+            <p class="tabContent_item--price" data-price ="${product.price}" data-id="${product.id}">
+              $${product.price}.00
+            </p>
+            <!-- end of price product-->
+          </div>
             `
     }
-    $('.shopContent_list .row ').html(item)
+  }
+  $('.shopContent_list .row ').html(item)
 }
-$(document).ready(function(){
-    $(".slider").owlCarousel({
-        animateIn: 'fadeIn',
-        animateOut: 'fadeOut',
-        slideSpeed: 100,
-        items:1,
-        margin:10,
-        autoHeight:true,
-        dots:false,
-        autoplay: true,
-        loop: true,
-    });
-    $(".featured_body--main").owlCarousel({
-      loop:true,
-      autoplay: true,
-      responsiveClass:true,
-      dots:false,
-      responsive:{
-          0:{
-              items:1,
-          },
-          390:{
-            items: 2,
-          },
-          400:{
-            items :2,
-          },
-          600:{
-              items:3,
-          },
-          1000:{
-              items:5,
-          },
-          1200:{
-              items:5,
-          },
-          1400:{
-              items:5,
-          }
+changePage()
+
+//slider of Website
+$(document).ready(function () {
+  $(".slider").owlCarousel({
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    slideSpeed: 100,
+    items: 1,
+    margin: 10,
+    autoHeight: true,
+    dots: false,
+    autoplay: true,
+    loop: true,
+  });
+  $(".featured_body--main").owlCarousel({
+    loop: true,
+    autoplay: true,
+    responsiveClass: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      390: {
+        items: 2,
+      },
+      400: {
+        items: 2,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+      1200: {
+        items: 5,
+      },
+      1400: {
+        items: 5,
       }
-  })
-    $('.lastNew_posts').owlCarousel({
-    loop:false,
-    margin:10,
-    nav:false,
-    dots:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        700:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
     }
-  })  
+  })
+  $('.lastNew_posts').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      700: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  })
   $('.testi_post').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots:true,
-    dotsEach:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    dotsEach: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
     }
   })
 
   $('.instagram_content').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots:true,
-    dotsEach:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:5
-        }
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    dotsEach: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 5
+      }
     }
   })
   $('.shopContent_slider').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots:true,
-    dotsEach:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    dotsEach: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
     }
   })
 })
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // ================== HEADER =========================
     //toggle header searchBar
     const searchButton = $(".searchItems")
     const header = $("#header")[0]
-    const searchbarMAin =$(".searchBar .searchBar--main")
+    const searchbarMAin = $(".searchBar .searchBar--main")
     const searchExitButton = $(".searchBar .searchBar--exit")
-    const bgSearchBar =$(".searchBar .bgSearchBar")
+    const bgSearchBar = $(".searchBar .bgSearchBar")
     const body = $("BODY")[0]
-    searchButton.on ("click",function(){
+    searchButton.on("click", function () {
         header.style.marginTop = "522px"
         searchbarMAin.toggleClass('active')
         bgSearchBar.toggleClass('active')
         $(body).toggleClass("activeBody")
     })
-    searchExitButton.on ("click",function(){
+    searchExitButton.on("click", function () {
         searchbarMAin.removeClass('active')
         header.style.marginTop = "0"
         bgSearchBar.removeClass('active')
@@ -22,76 +22,131 @@ $(document).ready(function() {
     })
     //toggle header cartBar
     const cartButton = $(".cartItems")
-    const cartBar =$(".cartBar")
-    const bgcartBar= $(".bgcartBar")
+    const cartBar = $(".cartBar")
+    const bgcartBar = $(".bgcartBar")
     const cartExit = $(".cartBar .cartBar_head--exit")
 
-    cartButton.on("click",function(){
+    cartButton.on("click", function () {
         cartBar.toggleClass('contentActive')
         bgcartBar.toggleClass('bgactive')
     })
-    cartExit.on("click",function(){
+    cartExit.on("click", function () {
         cartBar.toggleClass('contentActive')
         bgcartBar.toggleClass('bgactive')
     })
     //toggle  headBar
     const wishButton = $(".wishItems")
-    const wishBar =$(".wishBar")
-    const bgwishBar= $(".bgwishBar")
+    const wishBar = $(".wishBar")
+    const bgwishBar = $(".bgwishBar")
     const wishExit = $(".wishBar .wishBar_head--exit")
 
-    wishButton.on("click",function(){
+    wishButton.on("click", function () {
         wishBar.toggleClass('contentActive')
         bgwishBar.toggleClass('bgactive')
     })
-    wishExit.on("click",function(){
+    wishExit.on("click", function () {
         wishBar.toggleClass('contentActive')
         bgwishBar.toggleClass('bgactive')
     })
 
     // toggle menu responsive Mobile
     const opButton = $(".menuMoblie .menuMobile_left")
-    const clButton =$(".logoMenu")
-    const navMobile =$(".navMobile")
+    const clButton = $(".logoMenu")
+    const navMobile = $(".navMobile")
     const overlayMenu = $(".menu_overlay")
-    opButton.on("click",toggleNavMenu)
-    opButton.on("click",toggleOverlay)
-    clButton.on("click",toggleNavMenu)
-    clButton.on("click",toggleOverlay)
-    function toggleNavMenu(){
+    opButton.on("click", toggleNavMenu)
+    opButton.on("click", toggleOverlay)
+    clButton.on("click", toggleNavMenu)
+    clButton.on("click", toggleOverlay)
+    function toggleNavMenu() {
         navMobile.toggleClass('active')
     }
-    function toggleOverlay(){
+    function toggleOverlay() {
         overlayMenu.toggleClass('active')
     }
 
     // toggle menu Click outside
-    overlayMenu.on ("click",romeveActive)
-    bgwishBar.on("click",romeveActive)
-    bgcartBar.on("click",romeveActive)
-    bgSearchBar.on("click",romeveActive)
-    
+    overlayMenu.on("click", romeveActive)
+    bgwishBar.on("click", romeveActive)
+    bgcartBar.on("click", romeveActive)
+    bgSearchBar.on("click", romeveActive)
+
     function romeveActive(e) {
-        if(e.target== e.currentTarget)
-        {
-            navMobile.removeClass( 'active')
-            overlayMenu.removeClass( 'active')
-            wishBar.removeClass( 'contentActive')
-            bgwishBar.removeClass( 'bgactive')
-            cartBar.removeClass( 'contentActive')
-            bgcartBar.removeClass( 'bgactive')
-            bgSearchBar.removeClass( 'active')
-            searchbarMAin.removeClass( 'active')
+        if (e.target == e.currentTarget) {
+            navMobile.removeClass('active')
+            overlayMenu.removeClass('active')
+            wishBar.removeClass('contentActive')
+            bgwishBar.removeClass('bgactive')
+            cartBar.removeClass('contentActive')
+            bgcartBar.removeClass('bgactive')
+            bgSearchBar.removeClass('active')
+            searchbarMAin.removeClass('active')
             header.style.marginTop = "0px"
             $(body).removeClass("activeBody")
         }
     }
     // toggle bestSeller part
-    $('.bestSeller_options li').click(function(){
+    $('.bestSeller_options li').click(function () {
         let tab_id = $(this).attr('data-tab')
         $('.bestSeller_options li').removeClass('activeBtnTab')
         $('.tabContent li').removeClass('activeTab')
         $(this).addClass('activeBtnTab')
-        $("#"+tab_id).toggleClass('activeTab')
+        $("#" + tab_id).toggleClass('activeTab')
     })
+    // dropdown shoProducts
+    //change title of sorting
+    // ==================== Sorting Price & ID
+    function sort(val, param) {
+        let itemsShop = $(".shopItemsList")
+        itemsShop.sort(function (a, b) {
+            if (param == "") {
+                return (
+                    $(".tabContent_item--price", a).data(`${val}`) - $(".tabContent_item--price", b).data(`${val}`)
+                )
+            }
+            else{
+                return (
+                    $(".tabContent_item--price", b).data(`${val}`) - $(".tabContent_item--price", a).data(`${val}`)
+                )
+            }
+        })
+        $(".shopContent_list .row").append(itemsShop)
+    }
+    
+    // ==========================
+    function sortAlpha(val) {
+        let itemsShop = $(".shopItemsList")
+        itemsShop.sort((a, b) => {
+            const nameA = $(".tabContent_item--name", a).data("name").toUpperCase(); // ignore upper and lowercase
+            const nameB = $(".tabContent_item--name", b).data("name").toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) return val == "a2z" ? -1 : 1;
+            if (nameA > nameB) return val == "a2z" ? 1 : -1;
+            // names must be equal
+            return 0;
+        })
+        $(".shopContent_list .row").append(itemsShop)
+    }
+    // ==========================
+    function main() {
+        let sorting = $("#sort").val()
+        if (sorting == "h2l") {
+            sort('price',"hyhy")
+        }
+        else if (sorting == "l2h") {
+            sort('price',"")
+        }
+        else if (sorting == "default") {
+            sort('id',"")
+        }
+        else if (sorting == "a2z") {
+            sortAlpha("a2z")
+        }
+        else if (sorting == "z2a") {
+            sortAlpha("z2a")
+        }
+    }
+    $("#sort").click(function () {
+        main()
+    })
+
 })
